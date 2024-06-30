f483ed4930d7773d87559eaf95105e40a610b4d2
sysnote8main <sysnote8main@gmail.com>
2024-06-30 09:04:49 +0900

    initial commit

---
+++ src/client/java/com/sysnote8/systestmod/SysTestModClient.java
@@ -0,0 +1,10 @@
+package com.sysnote8.systestmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SysTestModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
